52a355e16b3e2194216f69be189973919ceaf664
student1rkgec <student1rkgec@gmail.com>
2018-04-21 04:22:05 -0400

    first push

---
+++ question.js
@@ -0,0 +1,50 @@
+var questions = [{
+	"question":" A 6 digit Hex color (#FF9966) defines values of Red, Blue and Green in which order ?",
+	"option1": "#BBRRGG",
+	"option2": "#BBGGRR",
+	"option3": "#RRGGBB",
+	"option4": "None of these",
+	"answer": "3"
+},
+{
+	"question":"When you count to 15 using hexadecimal numbers, the highest number is what ?",
+	"option1": "F",
+	"option2": "B",
+	"option3": "15",
+	"option4": "None of these",
+	"answer": "1"
+},
+{
+	"question":" The <small> and <big> tags are special in what way ?",
+	"option1": "They can be repeated",
+	"option2": "They work on anything",
+	"option3": "They are for images only",
+	"option4": "None of these",
+	"answer": "1"
+},
+{
+	"question":" What does vlink mean ?",
+	"option1": " visited link",
+	"option2": "very good link",
+	"option3": "active link",
+	"option4": "None of these",
+	"answer": "1"
+},
+{
+	"question":"Banners, buttons, dividers, clipart and other simple images usually work best as ?",
+	"option1": "fonts",
+	"option2": "gif",
+	"option3": "jpg",
+	"option4": "None of these",
+	"answer": "2"
+},
+{
+	"question":" A 6 digit Hex color (#FF9966) defines values of Red, Blue and Green in which order ?",
+	"option1": "#BBRRGG",
+	"option2": "#BBGGRR",
+	"option3": "#RRGGBB",
+	"option4": "None of these",
+	"answer": "3"
+}
+
+]
